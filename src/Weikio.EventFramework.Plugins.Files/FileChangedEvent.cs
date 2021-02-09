5573e345225d3c8485aa5b413c13dfc2b8b5f3c5
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-02-09 18:44:11 +0200

    Removed not needed project

--- src/Weikio.EventFramework.Plugins.Files/FileChangedEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Weikio.EventFramework.Files
-{
-    public class FileChangedEvent
-    {
-        
-    }
-}
--- src/Weikio.EventFramework.Plugins.Files/FileEventSourceConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Weikio.EventFramework.Plugins.Files
-{
-    public class FileEventSourceConfiguration
-    {
-        public string Folder { get; set; }
-        public string Filter { get; set; }
-    }
-}
