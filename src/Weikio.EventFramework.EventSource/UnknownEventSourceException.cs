7391569e9bd9e92b02c4a6907c6aa99371ba5381
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-02-16 12:35:33 +0200

    Fixed compilation

---
+++ src/Weikio.EventFramework.EventSource/UnknownEventSourceException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Weikio.EventFramework.EventSource
+{
+    public class UnknownEventSourceException : Exception
+    {
+        public UnknownEventSourceException(string message) : base(message)
+        {
+        }
+    }
+}
