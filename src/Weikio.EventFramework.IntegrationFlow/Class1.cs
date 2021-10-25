4c7c53065ecefb30763f884eb6e148ab236e2183
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-10-25 17:02:20 +0300

    Started working with the flow

---
+++ src/Weikio.EventFramework.IntegrationFlow/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Weikio.EventFramework.EventSource.Abstractions;
+
+namespace Weikio.EventFramework.IntegrationFlow
+{
+    public class IntegrationFlowBuilder : IBuilder<IntegrationFlow>
+    {
+        
+        public static IntegrationFlowBuilder From()
+        {
+            var builder = new IntegrationFlowBuilder();
+
+            return builder;
+        }
+
+        public static IntegrationFlowBuilder From<TEventSourceType>(Action<EventSourceInstanceOptions> configureInstance = null)
+        {
+            var builder = new IntegrationFlowBuilder();
+
+            return builder;
+        }
+
+        public Task<IntegrationFlow> Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IntegrationFlow
+    {
+        
+    }
+    public interface IBuilder<T> where T : class
+    {
+        Task<T> Build();
+    }
+}
---
+++ tests/integration/Weikio.EventFramework.IntegrationTests/Infrastructure/IntegrationFlowTestBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EventFrameworkTestBed;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Weikio.EventFramework.AspNetCore.Extensions;
+using Weikio.EventFramework.EventGateway;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Weikio.EventFramework.IntegrationTests.Infrastructure
+{
+    public abstract class IntegrationFlowTestBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        // Must be set in each test
+        public ITestOutputHelper Output { get; set; }
+        
+        protected IntegrationFlowTestBase(WebApplicationFactory<Startup> factory, ITestOutputHelper output)
+        {
+            Output = output;
+            _factory = factory;
+        }
+        
+        protected IServiceProvider Init(Action<IServiceCollection> action = null)
+        {
+            var result = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddEventFramework();
+                    action?.Invoke(services);
+                });
+
+                builder.ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); // Remove other loggers
+                    XUnitLoggerExtensions.AddXUnit((ILoggingBuilder) logging, Output); // Use the ITestOutputHelper instance
+                });
+
+            });
+
+            return result.Services;
+        }
+    }
+}
